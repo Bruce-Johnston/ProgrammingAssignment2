e66a4ad289cd050807e7ce5b3a44844bd89414a4
Bruce-Johnston <brucepaynejohnston@gmail.com>
2015-10-25 06:15:13 -0400

    My versions of the cachematrix function
    I hope this works out.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+xinverse <- NULL
+set <- function(y) {
+  x <<- y
+  xinverse <<- NULL
+}
+get <- function() x
+setinverse <- function(solve) xinverse <<- solve
+getinverse <- function() xinverse
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## I essentially copied the example code and hoped it was right.
+## I do not know if it is or not. I do not think so.
+## The <<- operator is new to me. It will take me a little
+## while to understand how this works, and I regret
+## to say that this assignment hit me during a very
+## busy weekend. I could not devote the time to it I
+## would like to have devoted. All apologies.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  xinverse <- x$solve()
+  if(!is.null(xinverse)) {
+    message("getting cached data")
+    return(xinverse)
+  }
+  NewMatrix <- x$get()
+  xinverse <- solve(NewMatrix, ...)
+  x$setinverse(xinverse)
+  xinverse
+  
 }
+##Again, no idea if this is going to work or not.
